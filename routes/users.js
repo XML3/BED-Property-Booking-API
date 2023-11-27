d5523cf5bf76fffcc1f9f140f16364da2209c0c4
Xagly Montilva <xaglymontilva@gmail.com>
2023-11-27 20:26:48 +0100

    created routes folder

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+import express from "express";
+import getUsers from "../src/service/users/getUsers.js";
+
+const router = express.Router();
+
+//Get All Users
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await getUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
