9c9de6dddbfed491918bfaf7fdf892cdf51c6c49
Xagly Montilva <xaglymontilva@gmail.com>
2023-11-29 21:07:51 +0100

    created Prisma Cliente for GET user by ID

---
+++ src/service/users/getUserById.js
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+const getUserById = async (id) => {
+  const prisma = new PrismaClient();
+
+  const user = await prisma.user.findUnique({
+    where: { id },
+  });
+
+  return user;
+};
+
+export default getUserById;
