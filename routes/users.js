27538a52d7d4585c1c799aa1f53d07c04bb4dc04
Xagly Montilva <xaglymontilva@gmail.com>
2023-11-29 20:41:35 +0100

    added POST route for create new user

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 import express from "express";
+import authMiddleware from "../src/middleware/auth.js";
 import getUsers from "../src/service/users/getUsers.js";
 
 const router = express.Router();
@@ -8,6 +9,25 @@ router.get("/", async (req, res, next) => {
   try {
     const users = await getUsers();
     res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//POST: Create New User
+router.post("/", authMiddleware, async (req, res, next) => {
+  try {
+    const { username, password, name, email, phoneNumber, profilePicture } =
+      req.body;
+    const newUser = await createUser(
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture
+    );
+    res.status(201).json(newUser);
   } catch (error) {
     next(error);
   }
