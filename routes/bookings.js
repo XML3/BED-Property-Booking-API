14f8bca5d7301de903dcfc97407c3a7820d7ff47
Xagly Montilva <xaglymontilva@gmail.com>
2023-11-30 20:05:40 +0100

    added routes for delete and get booking by ID

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,6 +1,8 @@
 import express from "express";
 import authMiddleware from "../src/middleware/auth.js";
 import getBookings from "../src/service/bookings/getBookings.js";
+import deleteBooking from "../src/service/bookings/deleteBookingById.js";
+import getBookingById from "../src/service/bookings/getBookingById.js";
 
 const router = express.Router();
 
@@ -43,3 +45,37 @@ router.post("/", authMiddleware, async (req, res, next) => {
 });
 
 //DELETE: Booking by ID
+router.delete("/:id", authMiddleware, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deleteBookingById = await deleteBooking(id);
+
+    if (!deleteBookingById) {
+      res.status(404).send(`Booking with id ${id} was not found`);
+    } else {
+      res
+        .status(200)
+        .json({ message: `Booking with id ${deleteBookingById} was deleted!` });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+//GET: Booking By ID
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const booking = await getBookingById(id);
+
+    if (!booking) {
+      res.status(404).send(`Booking with id ${id} was not found!`);
+    } else {
+      res.status(200).json(booking);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
