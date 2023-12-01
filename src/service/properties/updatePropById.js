69a49f382ce07193fe2a3737e01ed377e4708f99
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-01 03:21:13 +0100

    created Prisma Client Update Property by ID

---
+++ src/service/properties/updatePropById.js
@@ -0,0 +1,21 @@
+import { PrismaClient } from "@prisma/client";
+
+const updatePropertyById = async (id, updatedProperty) => {
+  const prisma = PrismaClient();
+
+  const { hostId, ...rest } = updatedProperty;
+
+  const propertyUpdate = await prisma.property.update({
+    where: { id },
+    data: {
+      ...rest,
+      host: {
+        connect: { id: hostId },
+      },
+    },
+  });
+
+  return propertyUpdate;
+};
+
+export default updatePropertyById;
