3f46bc06e6bf55dfdc14fb0de503ce5b336acc96
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-01 19:30:29 +0100

    corrected deleteMany which caused starnge behavior

--- src/service/reviews/deleteReviewById.js
+++ src/service/reviews/deleteReviewById.js
@@ -3,15 +3,11 @@ import { PrismaClient } from "@prisma/client";
 const deleteReview = async (id) => {
   const prisma = new PrismaClient();
 
-  const reviewDeleted = await prisma.review.deleteMany({
+  const reviewDeleted = await prisma.review.delete({
     where: { id },
   });
 
-  if (!reviewDeleted.count > 0) {
-    return id;
-  } else {
-    return null;
-  }
+  return reviewDeleted;
 };
 
 export default deleteReview;
