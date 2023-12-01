e091127ae39ad80e2155eeba731e145ed683bbfd
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-01 02:24:57 +0100

    added POST route for Create New Property

--- routes/properties.js
+++ routes/properties.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authMiddleware from "../src/middleware/auth.js";
 import getProperties from "../src/service/properties/getProperties.js";
+import createProperty from "../src/service/properties/createProperty.js";
 
 const router = express.Router();
 
@@ -15,5 +16,37 @@ router.get("/", async (req, res, next) => {
 });
 
 //POST: Create New Property
+router.post("/", async (req, res, next) => {
+  try {
+    const {
+      title,
+      description,
+      location,
+      pricePerNight,
+      bedroomCount,
+      bathRoomCount,
+      maxGuestCount,
+      hostId,
+      rating,
+    } = req.body;
+
+    const newProperty = await createProperty(
+      title,
+      description,
+      location,
+      pricePerNight,
+      bedroomCount,
+      bathRoomCount,
+      maxGuestCount,
+      hostId,
+      rating
+    );
+    res.status(200).json(newProperty);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//DELETE: Property by ID
 
 export default router;
