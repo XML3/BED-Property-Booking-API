2fa30a24c97ed3783614deb49c9fef7b59be6d8b
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-01 14:45:07 +0100

    created PrismaClient - Get All Hosts

---
+++ src/service/hosts/getHosts.js
@@ -0,0 +1,10 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+
+const getHosts = async () => {
+  const prisma = new PrismaClient();
+  const hosts = await prisma.host.findMany();
+
+  return hosts;
+};
+
+export default getHosts;
