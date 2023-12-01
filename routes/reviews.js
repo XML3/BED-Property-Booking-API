1a021deb5d8afdd37b4c60725f45a01e103031f7
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-01 18:34:45 +0100

    added route DELETE review by ID, correct error

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,6 +2,7 @@ import express from "express";
 import authMiddleware from "../src/middleware/auth.js";
 import getReviews from "../src/service/reviews/getReviews.js";
 import createReview from "../src/service/reviews/createReview.js";
+import deleteReview from "../src/service/reviews/deleteReviewById.js";
 
 const router = express.Router();
 
@@ -20,11 +21,27 @@ router.post("/", async (req, res, next) => {
   try {
     const { userId, propertyId, rating, comment } = req.body;
     const review = await createReview(userId, propertyId, rating, comment);
-    res.status(200).json(review);
+    res.status(201).json(review);
   } catch (error) {
     next(error);
   }
 });
 
 //DELETE: Review by ID
+router.delete("/:id", authMiddleware, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const deletedReviewById = await deleteReview(id);
+
+    if (!deletedReviewById) {
+      res.status(404).send(`Review with id ${id} not found! `);
+    } else {
+      res.status(200).json({
+        message: ` Review with id ${deletedReviewById} was successfully deleted!`,
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 export default router;
