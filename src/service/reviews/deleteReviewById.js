375ed94094e80982119b1a9d94d2cc8be5d470ca
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-01 18:35:10 +0100

    created PrismaClient for DELETE Review by ID

---
+++ src/service/reviews/deleteReviewById.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+const deleteReview = async (id) => {
+  const prisma = new PrismaClient();
+
+  const reviewDeleted = await prisma.review.deleteMany({
+    where: { id },
+  });
+
+  if (!reviewDeleted.count > 0) {
+    return id;
+  } else {
+    return null;
+  }
+};
+
+export default deleteReview;
