c01325c8a46f0aa7918ae051aafb8cb64fa5c194
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-01 19:01:36 +0100

    added routes for Get Review and Update Review ID

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,6 +3,8 @@ import authMiddleware from "../src/middleware/auth.js";
 import getReviews from "../src/service/reviews/getReviews.js";
 import createReview from "../src/service/reviews/createReview.js";
 import deleteReview from "../src/service/reviews/deleteReviewById.js";
+import getReviewById from "../src/service/reviews/getReviewById.js";
+import updateReviewById from "../src/service/reviews/updateReviewById.js";
 
 const router = express.Router();
 
@@ -44,4 +46,47 @@ router.delete("/:id", authMiddleware, async (req, res, next) => {
     next(error);
   }
 });
+
+//GET Review by ID
+router.get("/:id", authMiddleware, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const reviewById = await getReviewById(id);
+
+    if (!reviewById) {
+      res.status(404).send(`Review with id ${id} not found!`);
+    } else {
+      res.status(200).json(reviewById);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+//UPDATE: Review by ID
+router.put("/:id", authMiddleware, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { userId, propertyId, rating, comment } = req.body;
+
+    const review = await updateReviewById(id, {
+      userId,
+      propertyId,
+      rating,
+      comment,
+    });
+
+    if (review) {
+      res.status(200).send({
+        message: `Review with id ${id} successfully updated!`,
+        review,
+      });
+    } else {
+      res.status(404).json({ message: `Review with id ${id} not found!` });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
 export default router;
