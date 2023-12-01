e947538ab333ad8fc6a0a52dbd35d4d64ef885a1
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-01 02:10:04 +0100

    removed authMiddleware in Create New User

--- routes/users.js
+++ routes/users.js
@@ -19,7 +19,7 @@ router.get("/", async (req, res, next) => {
 });
 
 //POST: Create New User
-router.post("/", authMiddleware, async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   try {
     const { username, password, name, email, phoneNumber, profilePicture } =
       req.body;
