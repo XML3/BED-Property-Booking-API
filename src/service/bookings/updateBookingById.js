0c3e821f2ebf8c2f15a385380a1d5fd0490d15cd
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-04 18:27:45 +0100

    modified and removed connect to user and property

--- src/service/bookings/updateBookingById.js
+++ src/service/bookings/updateBookingById.js
@@ -3,19 +3,11 @@ import { PrismaClient } from "@prisma/client";
 const updateBookingById = async (id, updatedBooking) => {
   const prisma = new PrismaClient();
 
-  const { userId, propertyId, ...rest } = updatedBooking;
+  // const { userId, propertyId, ...rest } = updatedBooking;
 
   const booking = await prisma.booking.update({
     where: { id },
-    data: {
-      ...rest,
-      user: {
-        connect: { id: userId },
-      },
-      property: {
-        connect: { id: propertyId },
-      },
-    },
+    data: updatedBooking,
   });
 
   return booking;
