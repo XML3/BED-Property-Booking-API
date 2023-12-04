af153d8b47a053bea6e44b3ff184ce46f68b3376
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-04 18:29:16 +0100

    removed user and property connect id

--- src/service/reviews/updateReviewById.js
+++ src/service/reviews/updateReviewById.js
@@ -3,19 +3,11 @@ import { PrismaClient } from "@prisma/client";
 const updateReviewById = async (id, updatedReview) => {
   const prisma = new PrismaClient();
 
-  const { userId, propertyId, ...rest } = updatedReview;
+  // const { userId, propertyId, ...rest } = updatedReview;
 
   const reviewUpdated = await prisma.review.update({
     where: { id },
-    data: {
-      user: {
-        connect: { id: userId },
-      },
-      property: {
-        connect: { id: propertyId },
-      },
-      ...rest,
-    },
+    data: updatedReview,
   });
 
   return reviewUpdated;
