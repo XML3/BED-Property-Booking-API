c5940b7489290b6be3f0fce67f5f1ba6a6ff69f1
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-05 15:52:20 +0100

    removed commented out section of previous mod

--- src/service/properties/updatePropById.js
+++ src/service/properties/updatePropById.js
@@ -3,8 +3,6 @@ import { PrismaClient } from "@prisma/client";
 const updatePropertyById = async (id, updatedProperty) => {
   const prisma = new PrismaClient();
 
-  //const { hostId, ...rest } = updatedProperty;
-
   const propertyUpdate = await prisma.property.update({
     where: { id },
     data: updatedProperty,
