699bef209f52cdf7fde4ee92e3c44cee010b044a
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-05 17:45:23 +0100

    added select to try fix an error, not working X

--- src/service/reviews/getReviews.js
+++ src/service/reviews/getReviews.js
@@ -2,7 +2,15 @@ import { PrismaClient } from "@prisma/client";
 
 const getReviews = async () => {
   const prisma = new PrismaClient();
-  const reviews = await prisma.review.findMany();
+  const reviews = await prisma.review.findMany({
+    select: {
+      id: true,
+      userId: true,
+      propertyId: true,
+      rating: true,
+      comment: true,
+    },
+  });
 
   return reviews;
 };
