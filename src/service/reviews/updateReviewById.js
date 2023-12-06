221c2d04da286dd0d9b558a3e60e86eef65abe0e
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-06 17:50:37 +0100

    modified back to updateMany and added count block

--- src/service/reviews/updateReviewById.js
+++ src/service/reviews/updateReviewById.js
@@ -3,14 +3,16 @@ import { PrismaClient } from "@prisma/client";
 const updateReviewById = async (id, updatedReview) => {
   const prisma = new PrismaClient();
 
-  // const { userId, propertyId, ...rest } = updatedReview;
-
-  const reviewUpdated = await prisma.review.update({
+  const reviewUpdated = await prisma.review.updateMany({
     where: { id },
     data: updatedReview,
   });
 
-  return reviewUpdated;
+  if (reviewUpdated.count > 0) {
+    return id;
+  } else {
+    return null;
+  }
 };
 
 export default updateReviewById;
