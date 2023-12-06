2175e49c93e123612e463e8f906903bfc7847971
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-06 17:49:47 +0100

    modified back to updateMany and added count block

--- src/service/bookings/updateBookingById.js
+++ src/service/bookings/updateBookingById.js
@@ -5,12 +5,16 @@ const updateBookingById = async (id, updatedBooking) => {
 
   // const { userId, propertyId, ...rest } = updatedBooking;
 
-  const booking = await prisma.booking.update({
+  const booking = await prisma.booking.updateMany({
     where: { id },
     data: updatedBooking,
   });
 
-  return booking;
+  if (booking.count > 0) {
+    return id;
+  } else {
+    return null;
+  }
 };
 
 export default updateBookingById;
