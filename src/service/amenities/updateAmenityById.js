296b03f7cd6b9189ee7bbfaffa9e01e9f1a30098
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-06 17:49:16 +0100

    modifed back to updateMany and added missing code

--- src/service/amenities/updateAmenityById.js
+++ src/service/amenities/updateAmenityById.js
@@ -3,12 +3,12 @@ import { PrismaClient } from "@prisma/client";
 const updateAmenityById = async (id, updatedAmenity) => {
   const prisma = new PrismaClient();
 
-  const amenityUpdated = await prisma.amenity.update({
+  const amenityUpdated = await prisma.amenity.updateMany({
     where: { id },
     data: updatedAmenity,
   });
 
-  if (amenityUpdated) {
+  if (amenityUpdated.count > 0) {
     return id;
   } else {
     return null;
