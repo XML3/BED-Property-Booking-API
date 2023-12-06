9a1c6c99e2926195e13827f3c76c1feb663fd44d
Xagly Montilva <xaglymontilva@gmail.com>
2023-12-06 17:50:11 +0100

    modifed back to updateMany and added count block

--- src/service/properties/updatePropById.js
+++ src/service/properties/updatePropById.js
@@ -3,12 +3,16 @@ import { PrismaClient } from "@prisma/client";
 const updatePropertyById = async (id, updatedProperty) => {
   const prisma = new PrismaClient();
 
-  const propertyUpdate = await prisma.property.update({
+  const propertyUpdate = await prisma.property.updateMany({
     where: { id },
     data: updatedProperty,
   });
 
-  return propertyUpdate;
+  if (propertyUpdate.count > 0) {
+    return id;
+  } else {
+    return null;
+  }
 };
 
 export default updatePropertyById;
