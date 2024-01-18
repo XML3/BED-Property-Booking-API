7cffb86b147994033a4b45e19b0d1427fdb94dc5
Xagly Montilva <xaglymontilva@gmail.com>
2024-01-18 19:04:56 +0100

    modified  to query throught `userId`

--- src/service/bookings/getBookings.js
+++ src/service/bookings/getBookings.js
@@ -1,8 +1,24 @@
 import { PrismaClient } from "@prisma/client";
 
-const getBookings = async () => {
+const getBookings = async (userId) => {
   const prisma = new PrismaClient();
-  const bookings = await prisma.booking.findMany();
+  const bookings = await prisma.booking.findMany({
+    where: {
+      userId: {
+        contains: userId,
+      },
+    },
+    select: {
+      id: true,
+      userId: true,
+      propertyId: true,
+      checkinDate: true,
+      checkoutDate: true,
+      numberOfGuests: true,
+      totalPrice: true,
+      bookingStatus: true,
+    },
+  });
 
   return bookings;
 };
