00cec7d2cfeeccb5133401df3d81f01ed533d6fd
Xagly Montilva <xaglymontilva@gmail.com>
2024-01-18 17:55:15 +0100

    modified to fetch by query, set passward to false

--- src/service/hosts/getHosts.js
+++ src/service/hosts/getHosts.js
@@ -1,8 +1,26 @@
 import { PrismaClient } from "@prisma/client";
 
-const getHosts = async () => {
+const getHosts = async (username, email) => {
   const prisma = new PrismaClient();
-  const hosts = await prisma.host.findMany();
+  const hosts = await prisma.host.findMany({
+    where: {
+      username: {
+        contains: username,
+      },
+      email: {
+        contains: email,
+      },
+    },
+    select: {
+      id: true,
+      username: true,
+      password: false,
+      name: true,
+      email: true,
+      phoneNumber: true,
+      aboutMe: true,
+    },
+  });
 
   return hosts;
 };
