26c5bbad99fcf9e5314f7c6437e5b6864a7a583c
Xagly Montilva <xaglymontilva@gmail.com>
2024-01-19 01:24:07 +0100

    modified code to query by name

--- src/service/amenities/getAmenities.js
+++ src/service/amenities/getAmenities.js
@@ -1,8 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-const getAmenities = async () => {
+const getAmenities = async (name) => {
   const prisma = new PrismaClient();
-  const amenities = await prisma.amenity.findMany();
+  const amenities = await prisma.amenity.findMany({
+    where: {
+      name: {
+        contains: name,
+      },
+    },
+    select: {
+      id: true,
+      name: true,
+    },
+  });
 
   return amenities;
 };
