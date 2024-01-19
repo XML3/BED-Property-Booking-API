8687e10ece69b00156d38d630d3e8ae81e8c5ee8
Xagly Montilva <xaglymontilva@gmail.com>
2024-01-19 01:25:01 +0100

    modifed error in pricePerNight.  changed to equals: instead of contains: (which does not work with numbers)

--- src/service/properties/getProperties.js
+++ src/service/properties/getProperties.js
@@ -2,13 +2,14 @@ import { PrismaClient } from "@prisma/client";
 
 const getProperties = async (location, pricePerNight) => {
   const prisma = new PrismaClient();
+
   const properties = await prisma.property.findMany({
     where: {
       location: {
         contains: location,
       },
       pricePerNight: {
-        constains: pricePerNight,
+        equals: pricePerNight,
       },
     },
     select: {
