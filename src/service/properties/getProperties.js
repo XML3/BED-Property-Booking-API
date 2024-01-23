12fae21b890dd9e376f8db62bdf555c4758529f3
Xagly Montilva <xaglymontilva@gmail.com>
2024-01-23 14:44:54 +0100

    after connecting `amenities` in the prisma database, the conditionals will work.  Now amenities and conditionals are added to the code

--- src/service/properties/getProperties.js
+++ src/service/properties/getProperties.js
@@ -1,16 +1,29 @@
 import { PrismaClient } from "@prisma/client";
 
-const getProperties = async (location, pricePerNight) => {
+const getProperties = async (location, pricePerNight, amenities) => {
   const prisma = new PrismaClient();
 
   const properties = await prisma.property.findMany({
     where: {
-      location: {
-        contains: location,
-      },
-      pricePerNight: {
-        equals: pricePerNight,
-      },
+      ...(location && {
+        location: {
+          contains: location,
+        },
+      }),
+      ...(pricePerNight && {
+        pricePerNight: {
+          equals: pricePerNight,
+        },
+      }),
+      ...(amenities && {
+        amenities: {
+          some: {
+            name: {
+              contains: amenities,
+            },
+          },
+        },
+      }),
     },
     select: {
       id: true,
